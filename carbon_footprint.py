d321d125a424c0d9bbf00e72f1361d26d8bd07ee
deep-wind <78983104+deep-wind@users.noreply.github.com>
2023-01-24 03:58:16 +0530

    Update carbon_footprint.py

--- carbon_footprint.py
+++ carbon_footprint.py
@@ -47,7 +47,7 @@ try:
     nav = st.sidebar.radio("",["Home 🏡","Individual Emissions 👨‍👩‍👧‍👦","Group Emissions 🌐"])
     if nav == "Home 🏡":
       add_bg_from_local("back.jpg")
-      st.markdown("<h1 style ='color:#BB1D3F;text-align: center;font-family:times new roman;font-weight: bold;font-size:32pt;'>DEEP CARE 🌍❤️️ </h1>", unsafe_allow_html=True)  
+      
       st.markdown("<h1 style='color:black;text-align: center;font-family:times new roman;font-size:20pt;font-weight: bold;'>A CARBON FOOTPRINT CALCULATOR</h1>", unsafe_allow_html=True)
       st.markdown("<h1 style='color:green;text-align: center;font-family:times new roman;font-size:25pt;font-weight: bold;'>Reduce your carbon footprint!</h1>", unsafe_allow_html=True)        
    
